d642fff1152392ad96b4272513efd048fb2af6e1
Putra, A. R. Kusuma <48973489+arifaldi13@users.noreply.github.com>
2026-06-30 14:53:36 +0700

    Support Vercel root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/color-picker/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VERCEL ? '/' : '/color-picker/',
+    plugins: [react()],
+  }
 })
